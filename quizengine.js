6e2cdf03ab6faae8bdfb84669be746df62272117
Charles Hoot <hoot@nwmissouri.edu>
2021-05-21 16:59:44 -0500

    Update quizengine.js
    bug fix

--- quizengine.js
+++ quizengine.js
@@ -1,7 +1,7 @@
 let questionNumber = length(quizData);
 
 function showQuestion(i){
-    let target = document.getElementById("target")
+    let target = document.getElementById("target");
     target.innerhtml = "new things";
     alert("Button pressed")
 }
