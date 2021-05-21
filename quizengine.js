47ef5d6dcbf0de3f4744cfa308e03ddb11868725
Charles Hoot <hoot@nwmissouri.edu>
2021-05-21 16:55:19 -0500

    Update quizengine.js

--- quizengine.js
+++ quizengine.js
@@ -1,6 +1,7 @@
 let questionNumber = len(quizData);
 
 function showQuestion(i){
-    let target = document.findElementById("target")
-    target.innerhtml = "new things"
+    let target = document.findElementById("target");
+    target.innerhtml = "new things";
+    alert("Button pressed")
 }
