29de2d90dd198d1b9091b26cbdb96218962f5ff3
Charles Hoot <hoot@nwmissouri.edu>
2021-05-21 17:40:47 -0500

    Start quiz functionality
    Start quiz

--- quizengine.js
+++ quizengine.js
@@ -1,5 +1,12 @@
 let questionNumber = quizData.length
+let currentQuestion = 0;
 
+function startQuiz(){
+    let question = quizData[currentQuestion]
+    let questionBody = question[0];
+    let questionBodyElement = document.getElementById("quesitonBody");
+    questionBodyElement.innerHTML = questionBody
+}
 function showQuestion(i){
     alert("Button pressed");
     let target = document.getElementById("target");
